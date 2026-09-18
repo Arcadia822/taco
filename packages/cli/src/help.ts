4cddbaf805b14d48b69e0cc04dd651cffdc436eb
Arcadia <Arcadia822@gmail.com>
2026-09-18 19:07:07 +0800

    chore(cli): bump @tacobin/cli to 0.1.2

--- packages/cli/src/help.ts
+++ packages/cli/src/help.ts
@@ -1,6 +1,6 @@
 export const CLI_SCHEMA_HELP = 'taco-cli-help/1'
 export const CLI_SCHEMA_SKILLS = 'taco-cli-skills/1'
-export const CLI_BINARY_VERSION = '0.1.1'
+export const CLI_BINARY_VERSION = '0.1.2'
 export const DEFAULT_HOST = 'http://localhost:32167'
 
 export interface HelpPositional {
